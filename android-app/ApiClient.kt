ac2b771f10a87b0915e1671b81da414214aa6548
neilroman <neilromanmartinez@gmail.com>
2025-06-21 19:56:34 +0200

    Update ApiClient.kt

--- android-app/ApiClient.kt
+++ android-app/ApiClient.kt
@@ -1,16 +1,25 @@
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.POST
+import okhttp3.OkHttpClient
+import okhttp3.ResponseBody
+import retrofit2.http.Streaming
+
 interface ApiClient {
-    @POST("/getFormats")
+
+    @POST("getFormats")
     suspend fun getFormats(@Body body: Map<String, String>): List<FormatItem>
 
     @Streaming
-    @POST("/download")
+    @POST("download")
     suspend fun download(@Body body: Map<String, String>): ResponseBody
 
     companion object {
         fun create(): ApiClient {
             val client = OkHttpClient.Builder().build()
             return Retrofit.Builder()
-                .baseUrl("https://tu-backend-en-render.com/")
+                .baseUrl("https://yt-dlp-backend-8sqc.onrender.com/") // 🔁 Aquí tu backend real
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
                 .build()
